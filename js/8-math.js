d2fa428003f313233883ab1660bd0c6296df11da
Maciej Jarnicki <mjarnicki@gmail.com>
2018-04-22 14:27:34 +0200

    8-math zrobione

--- js/8-math.js
+++ js/8-math.js
@@ -4,3 +4,50 @@ console.log(Math.PI);
 console.log(Math.cos(0));
 
 console.log(Math.E);
+
+var liczba1 = 5;
+var liczba2 = 13;
+
+var wynik;
+
+wynik = liczba2 % liczba1;
+
+console.log(wynik);
+console.log(--wynik);
+
+console.log(wynik = wynik + 3);
+console.log(wynik += 3);
+
+if (wynik == 7)
+    {
+        console.log("PRAWDA");
+    }
+else
+    {
+        console.log("FAŁSZ");
+    }
+if ("2" == 2)
+    {
+        console.log("PRAWDA");
+    }
+else
+    {
+        console.log("FAŁSZ");
+    }
+if ("2" == 2)
+    {
+        console.log("PRAWDA");
+    }
+else
+    {
+        console.log("FAŁSZ");
+    }
+
+(3>4) ? console.log("prawda") : console.log("fałsz");
+
+
+
+
+
+
+
